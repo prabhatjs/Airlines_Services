f75ac289662e5da220081bce9c82d3ac8ca58489
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-01-15 00:01:44 +0530

    Add service function for airports

---
+++ src/services/airport_service.js
@@ -0,0 +1,62 @@
+
+const {StatusCodes}= require('http-status-codes');
+const {AirportRepo}=require('../repositories');
+
+const AirportObj=new AirportRepo();
+
+async function CreateAirport(data){
+try {
+    const airport= await AirportObj.create(data);
+    return airport;
+} catch (error) {
+    throw error
+}
+}
+async function DeleteAirport(id){
+try {
+    const airport=await AirportObj.destroy(id);
+    return airport
+} catch (error) {
+    if(404==StatusCodes.NOT_FOUND)
+        {
+            throw new Error(StatusCodes.NOT_FOUND);
+        }
+        throw new Error('Can not fetch id the Airport',StatusCodes.INTERNAL_SERVER_ERROR);
+}
+}
+        async function UpdateAirport(id,data){
+        try {
+            const airport=await AirportObj.update(id,data);
+            return airport;
+        } catch (error) {
+            throw error;
+        }
+        }
+    async function FindAirport(id){
+    try {
+        const airport=await AirportObj.geta(id);
+        return airport;
+    } catch (error) {
+        if(404==StatusCodes.NOT_FOUND)
+            {
+                throw new Error(StatusCodes.NOT_FOUND);
+            }
+            throw new Error('Can not fetch id the Airport',StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+    }
+        async function FindAllAirports(data){
+        try {
+            const airport=await AirportObj.getAll(data);
+            return airport;
+        } catch (error) {
+            throw new Error('Can not fetch all the Airports',StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+        }
+module.exports={
+    CreateAirport,
+    DeleteAirport,
+    UpdateAirport,
+    FindAirport,
+    FindAllAirports
+}
+//--> go to controller
